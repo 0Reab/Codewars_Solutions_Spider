156faa11d6c7c1dd92738d0f609dff255784f3c0
0Reab <122540000+0Reab@users.noreply.github.com>
2025-01-30 19:23:17 +0100

    Delete 6_kyu directory

--- 6_kyu/Count_characters_in_your_string.py
+++
@@ -1,8 +0,0 @@
-'''
-The main idea is to count all the occurring characters in a string. If you have a string like `aba`, then the result should be `{'a': 2, 'b': 1}`.
-
-What if the string is empty? Then the result should be empty object literal, `{}`.
-'''
-
-def count(s):
-    return {i: s.count(i) for i in set(s)}
